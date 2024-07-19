a55f4b691e01943162e6279bfa898727c28fbe54
meiezbr <mayezbr@outlook.fr>
2024-07-19 21:25:17 +0100

    fixing some terraform issues

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,8 @@
 variable ihrm_vpc_cidr_block {}
 variable ihrm_subnet_cidr_block {}
+variable ihrm_region {}
 variable ihrm_subnet_avail_zone {}
 variable ihrm_env_prefix {}
 variable ip_addresses_range {}
 variable instance_type {}
-variable public_key_location {}
 variable image_name {}
